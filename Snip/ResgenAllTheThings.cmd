3dc854ddfc41441b4339f5e205494b855de9f3bb
David Rudie <d.rudie@gmail.com>
2014-07-07 11:24:04 -0400

    Automated resgen process
    Resgen is now run via a script to automate the entire process.  The
    script will need updated manually each time a new resource file is
    added.
    
    I also moved the application signing to an external script that is
    manually run afterwards.  This should make it so people can check out
    Snip, build it, and run it without issues.

---
+++ Snip/ResgenAllTheThings.cmd
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set sourceDir=%1
+set targetDir=%2
+
+set resgenPath="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\Bin\resgen.exe"
+
+set fileToResgen[0]="%sourceDir%Resources\Strings"
+set fileToResgen[1]="%sourceDir%Resources\Strings.de-AT"
+set fileToResgen[2]="%sourceDir%Resources\Strings.de-CH"
+set fileToResgen[3]="%sourceDir%Resources\Strings.de-DE"
+set fileToResgen[4]="%sourceDir%Resources\Strings.fr-FR"
+set fileToResgen[5]="%sourceDir%Resources\Strings.nl-NL"
+set fileToResgen[6]="%sourceDir%Resources\Strings.se-SE"
+
+%resgenPath% /compile !fileToResgen[0]!.txt !fileToResgen[1]!.txt !fileToResgen[2]!.txt !fileToResgen[3]!.txt !fileToResgen[4]!.txt !fileToResgen[5]!.txt !fileToResgen[6]!.txt
+
+rem for /l %%n in (0,1,6) do (
+rem     copy /Y !fileToResgen[%%n]!.resources "bin\Debug\Resources"
+rem )
+
+for /f "tokens=2 delims==" %%s in ('set fileToResgen[') do copy /Y %%s.resources "%targetDir%Resources"
