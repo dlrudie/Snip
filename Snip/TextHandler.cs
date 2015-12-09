22cf82a45f24d61f5cb4aeb27192b5d759497dd7
kamijoutouma <michaelfox648@gmail.com>
2015-12-09 16:48:57 -0500

    count whats added to the string after truncation
    If you add a space and 3 dots after limiting it to 128 the result will be 132 so instead limit to 124 to add these without crashing the application.
    Fixes Issue https://github.com/dlrudie/Snip/issues/67

--- Snip/TextHandler.cs
+++ Snip/TextHandler.cs
@@ -36,7 +36,7 @@ namespace Winter
         {
             if (!string.IsNullOrEmpty(text))
             {
-                int maxLength = 127; // 128 max length minus 1
+                int maxLength = 124; // 128 max length minus 4 to allow for a space and 3 dots without crashing the program
 
                 if (text.Length >= maxLength)
                 {
