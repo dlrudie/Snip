e3bf085397eaab92bf70e7b727e6c2421d9a2814
David Rudie <d.rudie@gmail.com>
2022-10-08 20:03:48 -0500

    Removed unused code

--- Snip/UnsafeNativeMethods.cs
+++ Snip/UnsafeNativeMethods.cs
@@ -39,13 +39,5 @@ namespace Winter
         internal static extern bool UnregisterHotKey(
             [In] [Optional] IntPtr windowHandle,
             [In] int id);
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        internal static extern IntPtr SendMessage(
-            [In] IntPtr windowHandle,
-            [In] uint message,
-            [In] IntPtr wParam,
-            [In] IntPtr lParam);
     }
 }
